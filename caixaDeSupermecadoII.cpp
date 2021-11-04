ed13b9dede15bece4cbf412900e3f6c859b9e3d7
Paulo César <93353706+pdossantosjunior@users.noreply.github.com>
2021-11-04 18:54:30 -0300

    for while
    explicação

--- caixaDeSupermecadoII.cpp
+++ caixaDeSupermecadoII.cpp
@@ -3,13 +3,59 @@
 #include <locale.h>
 using namespace std;
 
+	int numero=0, i, total;
+
 	int main(){
 
 	setlocale(LC_ALL,"portuguese");
 
-	cout<<"Abertura de caixa!"<<endl<<endl;
+	/*cout<<"Abertura de caixa!"<<endl<<endl;
 	
-	cout<<"Informe o valor dos produtos"<<endl;
+	cout<<"Informe o valor dos produtos"<<endl;*/
+	
+		
+	while (numero<=10){
+		
+		cout<<"O valor da variável é: "<<numero<<endl;
+		
+		//numero = numero+1;
+		numero++;		
+	}
+	
+	
+	for (i=10;i>=0;i=i-3 /*ou i++ ou i--*/){
+		
+		cout<<"O valor da variável é: "<<i<<endl;
+		
+	}
+	
+	cout<<"Digite o numero que deseja saber a tabuada: "<<endl;
+	cin>>numero;
+	cout<<endl;
+	
+	for (i=0;i<=10;i++){
+		
+		total=i*numero;
+		
+	cout<<numero<<" X "<<i<<" = "<<total<<endl;
+		
+	}
+	
+	cout<<"Digite o numero que deseja saber a tabuada: "<<endl;
+	cin>>numero;
+	cout<<endl;
+	
+	i=0;
+	
+	while (i<=10){
+		
+		total=i*numero;
+		
+	cout<<numero<<" X "<<i<<" = "<<total<<endl;
+	
+	//i=i+1;
+	i++;		
+	}
 
 
     system("pause");
