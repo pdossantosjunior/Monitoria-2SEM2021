758856b1e181c3e4dccfb19c52b9605ccfb09bbe
Paulo César <93353706+pdossantosjunior@users.noreply.github.com>
2021-11-30 18:53:24 -0300

    ArrayII
    ArrayII

---
+++ 20211130-ModeloArrayII.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+		int array[7], arraymatriz[7][5];
+	
+
+	int main(){
+		
+		cin>>arraymatriz[0][0];
+		cin>>arraymatriz[1][1];
+		cin>>array[6];
+		
+		
+		cout<<arraymatriz[0][0]<<endl;
+		cout<<arraymatriz[1][1]<<endl;
+		cout<<array[6]<<endl;	    
+
+	}
+	
+	
+	pessoa1[5];
+	pessoa2[5];
+	pessoa3[5];
+	variavel[10];
+	
+	
