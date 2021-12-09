5e01ac40d9b5bc0c611fd29d54b4337e89952f35
Paulo César <93353706+pdossantosjunior@users.noreply.github.com>
2021-12-09 20:46:48 -0300

    20211209-Exemplos
    20211209-Exemplos

---
+++ 20211209-Exemplos.cpp
@@ -0,0 +1,41 @@
+
+i=1;
+while(i<=24){
+	
+	
+	i++;
+}
+
+do{
+	
+	
+}while(i=0);
+
+
+
+
+
+for(i = 1; i <= 24; i++){
+                capital = capital + ((capital / 100) * jurosMensal); // Usei essa formula para facilitar a compreensão do calculo!
+                cout<<i<<"º mês | rendimento: R$ "<<capital<<endl;
+                //i++;
+            }
+            
+            
+            
+            
+            
+for(i = 1; i <= 24; i++)
+
+			i=1; i<=24;
+			
+			{
+	
+	
+                capital = capital + ((capital / 100) * jurosMensal); // Usei essa formula para facilitar a compreensão do calculo!
+                
+                
+                i++;
+            }
+            
+            cout<<i=25<<"º mês | rendimento: R$ "<<capital<<endl;
